3042c2bc39377f6a287dcf9c796b089fc0f070d1
Evgeniy Bilyk <bilyk@clickky.me>
2018-05-25 11:11:08 +0300

    Added lua dev packages; Added aerospike-client-c library;

---
+++ docker/openresty/aerospike-cl-lua/build_linux.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+gcc -I/usr/include/luajit-2.0/ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"src/as_lua.d" -MT"src/as_lua.d" -o "./src/as_lua.o" "src/as_lua.c" -std=gnu99 -g -rdynamic -Wall -fno-common -fno-strict-aliasing -fPIC -DMARCH_x86_64 -D_FILE_OFFSET_BITS=64 -D_REENTRANT -D_GNU_SOURCE
+gcc -shared -o "as_lua.so"  ./src/as_lua.o -laerospike -lssl -lcrypto -lpthread -lrt -lm
